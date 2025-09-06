b7de740d13da7c4de20c9543dbe13c634e3c919f
Vladislav Kotletkin <drzhkn1@mail.ru>
2025-09-06 20:21:46 +0300

    refactor: refactored interaction api module

--- core/interaction-api/src/main/java/ru/practicum/exception/ValidationException.java
+++ core/interaction-api/src/main/java/ru/practicum/exception/ValidationException.java
@@ -1,7 +1,23 @@
 package ru.practicum.exception;
 
-public class ValidationException extends Exception {
+import java.text.MessageFormat;
+import java.util.function.Supplier;
+
+public class ValidationException extends RuntimeException {
+
     public ValidationException(String message) {
         super(message);
+    }
+
+    public ValidationException(String message, Object... args) {
+        super(MessageFormat.format(message, args));
+    }
+
+    public static Supplier<ValidationException> validationException(String message, Object... args) {
+        return () -> new ValidationException(message, args);
+    }
+
+    public static Supplier<ValidationException> validationException(String message) {
+        return () -> new ValidationException(message);
     }
 }
--- core/interaction-api/src/main/java/ru/practicum/exception/WrongDataException.java
+++ core/interaction-api/src/main/java/ru/practicum/exception/WrongDataException.java
@@ -1,7 +1,23 @@
 package ru.practicum.exception;
 
-public class WrongDataException extends Exception {
+import java.text.MessageFormat;
+import java.util.function.Supplier;
+
+public class WrongDataException extends RuntimeException {
+
     public WrongDataException(String message) {
         super(message);
+    }
+
+    public WrongDataException(String message, Object... args) {
+        super(MessageFormat.format(message, args));
+    }
+
+    public static Supplier<WrongDataException> wrongDataException(String message, Object... args) {
+        return () -> new WrongDataException(message, args);
+    }
+
+    public static Supplier<WrongDataException> wrongDataException(String message) {
+        return () -> new WrongDataException(message);
     }
 }
